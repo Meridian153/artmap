11c92e8709bec33c3aef3998fd0291d1b4dfa4b7
Cori1304-Seong <andyjm7739@gmail.com>
2026-06-30 15:36:38 +0900

    refactor(db.ts): add env.DATABASE_URL error case

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,5 +8,8 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const neonClient = neon(process.env.DATABASE_URL!);
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL 환경변수가 설정되지 않았습니다.");
+}
+const neonClient = neon(process.env.DATABASE_URL);
 export const db = drizzle(neonClient, { schema });
